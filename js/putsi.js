55c964b02d350f634b1a83d3bdac2eb6bc20eb18
Jarmo Puttonen <github@rot.fi>
2021-07-29 14:01:09 +0300

    Update

--- js/putsi.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$(window).on("hashchange", function() {
-	   	var hash = window.location.hash.substring(1);
-	  	$(".nav-item").toggleClass("active", false);
-		$(".item-" + hash).toggleClass("active", true);
-	  	$("section").toggleClass("visible-print-block", true);
-	   	$("#" + hash).toggleClass("visible-print-block", false);
-	 });
-});
